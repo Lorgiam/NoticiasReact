e7f1b5cdc31e9655a2f856e16b6747b9a01ec090
Lorgiam Arce <lorgi-1026@hotmail.com>
2019-01-12 18:09:32 -0500

    Update Header.js

--- src/componentes/Header.js
+++ src/componentes/Header.js
@@ -6,7 +6,7 @@ const Header = props => {
     <nav>
       <div className='nav-wrapper light-blue darken-3'>
         <a href='false' className='brand-logo center'>
-          {props.titulo}
+          {props.titulo} - Creado por Lorgiam Arce Castaño
         </a>
       </div>
     </nav>
